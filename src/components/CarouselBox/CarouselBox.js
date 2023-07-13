8f7f71d04600173c3821155578f65eb0386c2205
Vlad <nikwlad11@gmail.com>
2023-07-13 22:44:00 +0200

    eddit tags h1 for main information

--- src/components/CarouselBox/CarouselBox.js
+++ src/components/CarouselBox/CarouselBox.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-// import CarouselImg1 from './a_call_to_action_for_generative_ai_cs_4.jpg';
-// import CarouselImg2 from './canadian_tire_corporation_and_microsoft_partner_to_advance_retail_innovation_in_canada_1920.jpg';
-// import CarouselImg3 from './canadian_tire_corporation_and_microsoft_partner_to_advance_retail_innovation_in_canada_1920.jpg';
 import './CarouselBox.css';
 
 const CarouselBox = () => {
@@ -13,7 +10,7 @@ const CarouselBox = () => {
                         <div className="carouselimg1"></div>
                     <Carousel.Caption className='carousel-h1 d-flex flex-column justify-content-center h-100'>
                     <div className="star">★★★★★</div>
-                    <h1 className="carousel-h1">Jesteśmy gotowi zaoferować Państwu sekretną formułę sukcesu</h1>
+                    <h1 className="carousel-h1">Przyciągamy 30% więcej zysków do twojej firmy korzystając z naszej tajnej formulę</h1>
                     <p className="p-gray">Uwolnij potencjał swoich branż cyfrowych</p>
                     <a className="carousel-a" href="/about">Read More →</a>
                     </Carousel.Caption>
@@ -21,15 +18,15 @@ const CarouselBox = () => {
                     <Carousel.Item interval={5000}>
                         <div className="carouselimg2"></div>
                     <Carousel.Caption className='carousel-h1 d-flex flex-column justify-content-center h-100'>
-                        <h1 className="carousel-h1">Dla świata definiowanego programowo</h1>
-                        <p className='p-gray'>Zapewniamy cyfrową przewagę organizacjom opartym na oprogramowaniu</p>
+                        <h1 className="carousel-h1">Wypróbuj naszą 4-etapową formułę IT już dziś</h1>
+                        <p className='p-gray'>Dla świata definiowanego programowo</p>
                         <a className="carousel-a" href="#whoweare">Read More →</a>
                     </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item interval={5000}>
                         <div className="carouselimg3"></div>
                     <Carousel.Caption className='carousel-h1 d-flex flex-column justify-content-center h-100'>
-                        <h1 className="carousel-h1">IQA Announces Strategic Global Partnership with Google Cloud</h1>
+                        <h1 className="carousel-h1">Poczuj unikalny proces pracy z klientem</h1>
                         <a className="carousel-a" href="/about">Read More →</a>
                     </Carousel.Caption>
                     </Carousel.Item>
