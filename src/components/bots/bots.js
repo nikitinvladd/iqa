b072062286abcf7f89016ea7847284afa8eac7c9
Vlad <nikwlad11@gmail.com>
2023-07-17 22:50:09 +0200

    eddit text for not all offers

--- src/components/bots/bots.js
+++ src/components/bots/bots.js
@@ -1,25 +1,19 @@
 import './bots.css';
-import { motion } from "framer-motion";
 
-const textAnimationOpacity = {
-    hidden: {
-        opacity: 0,
-    },  
-    visible: custom => ({
-        opacity: 1,
-        transition: { delay: custom * 0.2},
-    }),
-}
 const Bots = () => {
     return(
-        <motion.div viewport={{once: true}} initial="hidden" whileInView="visible" className="bots">
-            <motion.div custom={1} variants={textAnimationOpacity} className="bots-information">
+        <div className="bots">
+            <div className="bots-information">
                 <p className='p-red-bots'>B O T S</p>
-                <h1>BOTY</h1>
-                <p className='p-gray'>Potrzebujesz specjalistów w sfierze Software Enginnering? Nasz zespól przedstawi tobie najliepsze rozwiązanie twoich pomysłów!</p>
-            </motion.div>
-            <motion.div custom={3} variants={textAnimationOpacity} className="bots-img"><img src="/img/bot.png" alt="bots" /></motion.div>
-        </motion.div>
+                <h1>Tworzenie botów</h1>
+                <p className='p-gray'>Boty obejmują szeroką gamę transformacyjnych narzędzi, które mogą zrewolucjonizować Twoją cyfrową obecność. W tym boty parsujące, boty analityczne i boty dla platform mediów społecznościowych, oferują szeroki wachlarz funkcji, które usprawniają operacje biznesowe. Dowiedz się, w jaki sposób nasza usługa tworzenia botów może przynieść korzyści Twojej firmie:<br></br>
+                Firmy wykorzystujące technologię do rozwoju: Wykorzystaj moc botów, aby popchnąć swoją firmę do przodu. Dzięki naszemu doświadczeniu możesz wykorzystać najnowocześniejsze technologie, aby zwiększyć produktywność, poprawić doświadczenia klientów i napędzać zrównoważony wzrost.<br></br>
+                Firmy poszukujące dopasowanych rozwiązań: Nasze niestandardowe boty są zaprojektowane tak, aby spełniały unikalne wymagania biznesowe, zapewniając idealne dopasowanie do konkretnych potrzeb i celów.
+                Przedsiębiorcy i startupy budujące cyfrowy ślad: Zapewnij swojej firmie cyfrową przewagę, której potrzebuje. Nasze aplikacje botów mogą pomóc w ustanowieniu solidnej obecności online, ułatwiając skuteczne zaangażowanie docelowych odbiorców.
+                </p>
+            </div>
+            <div className="bots-img"><img src="/img/bot.png" alt="bots" /></div>
+        </div>
     )
 }
 export default Bots
--- src/components/websites/websites.js
+++ src/components/websites/websites.js
@@ -16,8 +16,12 @@ const Websites = () => {
         <motion.div viewport={{once: true}} initial="hidden" whileInView="visible" className="websites">
             <motion.div custom={1} variants={textAnimationOpacity} className="websites-information">
                 <p className="p-red-websites">W E B S I T E S</p>
-                <h1>Strony Internetowe</h1>
-                <p className='p-gray'>Przedstaw sobie lub swoj biznes swiatu, za pomocą indywidualnego design i tajny klucz sprzedaży od naszych reklamodawców.</p>
+                <h1>Tworzenie stron internetowych</h1>
+                <p className='p-gray'><b>Zaprezentuj swoje pomysły!</b> Strony internetowe to transformacyjne narzędzie, które może zrewolucjonizować Twoją obecność w Internecie. Te dynamiczne i interaktywne strony internetowe, dostępne za pośrednictwem przeglądarek internetowych, oferują szeroki zakres funkcji, które usprawniają operacje biznesowe. Dowiedz się, w jaki sposób nasza usługa tworzenia stron internetowych może przynieść korzyści Twojej firmie:<br></br>
+                <b>Indywidualne rozwiązania dla firm:</b> Nasze niestandardowe strony internetowe są zaprojektowane tak, aby spełniały unikalne wymagania biznesowe, zapewniając idealne dopasowanie do konkretnych potrzeb i celów.<br></br>
+                <b>Wspieranie rozwoju biznesu dzięki technologii:</b> Wykorzystaj moc tworzenia stron internetowych, aby popchnąć swoją firmę do przodu. Dzięki naszemu doświadczeniu możesz wykorzystać najnowocześniejsze technologie, aby zwiększyć produktywność, poprawić doświadczenia klientów i napędzać zrównoważony wzrost.<br></br>
+                <b>Przedsiębiorcy i startupy budujące swój cyfrowy ślad:</b> Zapewnij swojej firmie cyfrową przewagę, której potrzebuje. Nasze usługi tworzenia stron internetowych mogą pomóc w budowaniu silnej obecności online, umożliwiając skuteczne angażowanie i łączenie się z docelowymi odbiorcami.
+                </p>
             </motion.div>
             <motion.div custom={3.2} variants={textAnimationOpacity} className="websites-img"><img src="/img/webapp.png" alt="websites" /></motion.div>
         </motion.div>
